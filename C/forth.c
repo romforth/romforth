85a914d60ffce1f9835228f36ce6663844b6ceb1
Rom Embedded Forth <romforth@proton.me>
2023-02-01 05:19:40 -0800

    C Port : step 0 : A halting interpreter
    Restart the C port from scratch
    
    The JOURNAL entry goes into more detail but this is just a fresh start
    of the C port which had been put on indefinite hiatus earlier.
    
    Most of the scripts are just minor tweaks of the PDP11 equivalents.
    Additional changes will be made as and when necessary.
    
    This is adding on still more $TECHDEBT because of even more copy/paste of
    the Perl scripts. At some point I should try to unify the genrom and
    genprims scripts across ports.

--- C/forth.c
+++ C/forth.c
@@ -1,99 +1,24 @@
 /*
  * forth.c : inner interpreter and other glue logic
  *
- * Copyright (c) 2022 Charles Suresh <romforth@proton.me>
+ * Copyright (c) 2022-2023 Charles Suresh <romforth@proton.me>
  * SPDX-License-Identifier: AGPL-3.0-only
  * Please see the LICENSE file for the Affero GPL 3.0 license details
  */
 
-#include <stdlib.h>	// exit
-#include <stdio.h>	// getchar, putchar
-#include <stddef.h>	// offsetof
-
-typedef unsigned char byte;
-
-typedef short cell;
-
-cell tos, nos;
-
-struct mem {
-	union u {
-		struct {
-			cell here;
-			byte state;
-		} bss;
-		byte ram[1<<(8*sizeof(cell))];
-	} u;
-} memory;
-
-#include <stdarg.h>
-
-void
-_die(char *f, int l, int n, char *s, ...) {
-	va_list ap;
-
-	va_start(ap, s);
-	vfprintf(stderr, s, ap);
-	fprintf(stderr, "Died in file %s at line %d\n", f, l);
-	exit(n);
-}
-
-#define die(n, s, w) _die(__FILE__, __LINE__, n, s, w);
-
-#define MAXD 100
-cell dstk[MAXD], *d=dstk, maxd=0;
-
-void
-checkempty(cell *d, cell *s) {
-	if (d!=s) {
-		die(2, "Data stack is not empty, remaining=%d\n", d-s);
-	}
-}
+#include <stdlib.h>	// atexit
 
 void
 verify() {
-	checkempty(d, dstk);
-}
-
-void
-push(cell c) {
-	*d++=c;
-	int l=d-dstk;
-	if (l>maxd) {
-		maxd=l;
-	}
-	if (maxd>MAXD) {
-		die(3, "Data stack size needs to be increased, current=%d\n", MAXD);
-	}
-}
-
-void
-pop(cell *cp) {
-	*cp=*--d;
-	if (d-dstk < 0) {
-		die(4, "Data stack underflowed by %d\n", d-dstk);
-	}
-}
-
-void lit(cell);
-
-#include "prims.h"
-
-void
-lit(cell c) {
-	_dup;
-	tos=c;
-}
-
-#include "defs.h"
-
-void
-rom() {
-#include "rom.h"
 }
 
 int
 main() {
+	static const unsigned char rom[] = {
+#include "rom.h"
+	};
+	const unsigned char register *ip=rom;
+
 	atexit(verify);
-	rom();
+#include "prims.h"
 }
