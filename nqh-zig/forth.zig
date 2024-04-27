04ffa3714c6bbab65d291bd9e97f44c7907b8265
Rom Embedded Forth <romforth@proton.me>
2024-04-27 06:34:06 -0700

    nqh-zig : step 1 : Implement emit, and fake key for now

--- nqh-zig/forth.zig
+++ nqh-zig/forth.zig
@@ -4,17 +4,26 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Please see the LICENSE file for the Affero GPL 3.0 license details
 
+const std = @import("std");
 const rom = @import("rom.zig");
 
 var ip: usize = 0;
 var i: rom.Bytecode = undefined;
+var tos: i8 = undefined;
 
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
     out: while (true) {
         i = rom.bytes[ip];
         ip += 1;
         switch (i) {
             .bye => break :out,
+            .emit => {
+                try stdout.print("{c}", .{@as(u8, @intCast(tos))});
+            },
+            .key => {
+                tos = 'f';
+            },
         }
     }
 }
--- nqh-zig/rom.zig
+++ nqh-zig/rom.zig
@@ -1,7 +1,11 @@
 pub const Bytecode = enum(u8) {
+    emit,
+    key,
     bye,
 };
 
 pub const bytes = [_]Bytecode{
+	.key,
+	.emit,
 	.bye,
 };
