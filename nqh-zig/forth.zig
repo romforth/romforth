7dedcdce936cbcc282cd6f108347df3814e7036c
Rom Embedded Forth <romforth@proton.me>
2024-04-27 06:02:57 -0700

    nqh-zig : step 0.1 : A halting interpreter, with bytecodes

--- nqh-zig/forth.zig
+++ nqh-zig/forth.zig
@@ -4,4 +4,23 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // Please see the LICENSE file for the Affero GPL 3.0 license details
 
-pub fn main() !void {}
+const Bytecode = enum(u8) {
+    bye,
+};
+
+const rom = [_]Bytecode{
+    .bye,
+};
+
+var ip: usize = 0;
+var i: Bytecode = undefined;
+
+pub fn main() !void {
+    out: while (true) {
+        i = rom[ip];
+        ip += 1;
+        switch (i) {
+            .bye => break :out,
+        }
+    }
+}
